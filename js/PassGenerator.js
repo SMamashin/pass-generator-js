ac1fc783d7a2b8d7e7e2a84a448b6fd51cc02fe7
Stepan <107657774+SMamashin@users.noreply.github.com>
2024-01-11 07:18:16 +0300

    Add files via upload

--- js/PassGenerator.js
+++ js/PassGenerator.js
@@ -1,4 +1,4 @@
-function getPassword(){
+function getPassword16(){
 
     var chars = "0123456789101002281337ABCDISDFKLkdfjglsdfkJSDKLFJSKLDFJKLSDFJKLSDJFKLD";
     var passwordLength = 16;
@@ -10,3 +10,41 @@ function getPassword(){
     }
     document.getElementById("password").value = password
 }
+function getPassword8(){
+
+    var chars = "0123456789101002281337ABCDISDFKLkdfjglsdfkJSDKLFJSKLDFJKLSDFJKLSDJFKLD";
+    var passwordLength = 8;
+    var password = "";
+
+    for (var i=0; i<passwordLength; i++){
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber+1);
+    }
+    document.getElementById("password").value = password
+}
+function getPassword32(){
+
+    var chars = "0123456789101002281337ABCDISDFKLkdfjglsdfkJSDKLFJSKLDFJKLSDFJKLSDJFKLD";
+    var passwordLength = 32;
+    var password = "";
+
+    for (var i=0; i<passwordLength; i++){
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber+1);
+    }
+    document.getElementById("password").value = password
+}
+function getPasswordSymbols(){
+
+    var chars = "0123456789101002281337ABCDISDFKLkdfjglsdfkJSDKLFJSKLDFJKLSDFJKLSDJFKLD_-*%№@!$^?()";
+    var passwordLength = 16;
+    var password = "";
+
+    for (var i=0; i<passwordLength; i++){
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber+1);
+    }
+    document.getElementById("password").value = password
+}
+
+// lol
